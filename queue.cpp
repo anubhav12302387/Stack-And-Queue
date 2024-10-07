609b763f265233d536be789e30bd697bee0f50f9
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-07 14:57:01 +0530

    Circular Queue

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,5 @@
+// Basic Queue Problem
+
 #include<iostream>
 using namespace std;
 #define MAX 5
@@ -65,6 +67,94 @@ int main(){
                 return 0;
             default:
                 cout<<"Invalid choice"<<endl;
+        }
+    }
+}
+
+
+
+
+
+// Circular Queue
+
+
+#include<iostream>
+using namespace std;
+#define SIZE 5
+int queue[SIZE];
+int front = -1;
+int rear = -1;
+
+bool isFull(){
+    return ((rear + 1) % SIZE == front);
+}
+
+bool isEmpty(){
+    return (front == -1);
+}
+
+void enqueue(int value){
+    if (isFull()){
+        cout << "Queue overflow! Cannot Enqueue " << value << endl;
+        return;
+    }
+
+    if(front == -1){
+        front = 0;
+    }
+    rear = (rear + 1) % SIZE;
+    queue[rear] = value;
+    cout << "Enqueued value: " << value << endl;
+}
+
+void dequeue(){
+    if (isEmpty()) {
+        cout << "Queue Underflow" << endl;
+        return;
+    }
+
+    cout << "Dequeued value: " << queue[front] << endl;
+    if (front == rear) {
+        front = -1;
+        rear = -1;
+    } else {
+        front = (front + 1) % SIZE;
+    }
+}
+
+void display(){
+    if (isEmpty()) {
+        cout << "Queue is empty" << endl;
+    } else {
+        cout << "Current Queue: ";
+        int i = front;
+        while (i != rear) {
+            cout << queue[i] << " ";
+            i = (i + 1) % SIZE;
+        }
+        cout << queue[rear] << endl;
+    }
+}
+
+int main(){
+    int choice, value;
+    while(true){
+        cin >> choice;
+        switch(choice){
+            case 1:
+                cin >> value;
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout << "Invalid choice" << endl;
         }
     }
 }
