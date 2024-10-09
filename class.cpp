d3b0839fa354bdb59f873e487bbef5043d9b079f
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-09 10:51:24 +0530

    seond class

--- class.cpp
+++ class.cpp
@@ -40,3 +40,47 @@ int main(){
     obj1.display();
     return 0;
 }
+
+
+
+// You are using GCC
+#include<iostream>
+using namespace std;
+class B{
+    protected:
+    int x;
+    public:
+    void get_dataB()
+    {
+        cin>>x;
+    }
+};
+class DB1: public virtual bitand
+{
+    protected:
+    int z;
+    public:
+    void get_dataDB2()
+    {
+        cin>>z;
+    }
+};
+class D: public DB1, public get_dataDB2
+{
+    public:
+    void sum()
+    {
+        int result;
+        result=x+y+z;
+        cout<<"Result is:"<<result;
+    }
+};
+int main()
+{
+    D obj1;
+    obj1.get_dataB();
+    obj1.get_dataDB1();
+    obj1.get_dataDB2();
+    obj1.sum();
+    return 0;
+}
--- queue.cpp
+++ queue.cpp
@@ -158,3 +158,85 @@ int main(){
         }
     }
 }
+
+
+
+
+// Queue implementation using Linked list
+#include<iostream>
+using namespace std;
+
+struct node {
+    int data;
+    node *next;
+};
+
+node *front = nullptr;
+node *rear = nullptr;
+
+void enqueue(int value) {
+    node *newnode = new node();
+    newnode->data = value;
+    newnode->next = nullptr;
+    if (rear == nullptr) {
+        front = rear = newnode; 
+    } else {
+        rear->next = newnode; 
+        rear = newnode; 
+    }
+    cout << "Enqueued value: " << value << endl;
+}
+
+void dequeue() {
+    if (front == nullptr) {
+        cout << "Underflow" << endl;
+    } else {
+        node *temp = front; 
+        front = front->next; 
+        cout << "Dequeued value: " << temp->data << endl; 
+        delete temp; 
+    }
+}
+
+void display() {
+    if (front == nullptr) {
+        cout << "Queue is empty" << endl; 
+    } else {
+        node *current = front; 
+        cout << "Queue contents: ";
+        while (current != nullptr) {
+            cout << current->data << " "; 
+            current = current->next; 
+        }
+        cout << endl; 
+    }
+}
+
+int main() {
+    int choice, value;
+    while (true) {
+        cout << "1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: 
+                cout << "Enter value to enqueue: ";
+                cin >> value;
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4: 
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+    return 0;
+}
