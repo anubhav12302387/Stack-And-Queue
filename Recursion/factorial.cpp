3771f68d7524cb6585129550ce4099c6b60e3924
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-11 15:23:53 +0530

    sum of arrays

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -23,30 +23,60 @@
 
 // fibonaci series
 
+// #include<iostream>
+// using namespace std;
+
+// int fibonacci(int n) {
+//     if (n == 0) {
+//         return 0;
+//     } else if (n == 1) {
+//         return 1;
+//     } else {
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
+
+// int main() {
+//     int terms;
+//     cout << "Enter the number of terms: ";
+//     cin >> terms;
+
+//     cout << "Fibonacci Series: ";
+//     for (int i = 0; i < terms; i++) {
+//         cout << fibonacci(i) << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
+
+//sum of array elements
+
 #include<iostream>
 using namespace std;
 
-int fibonacci(int n) {
-    if (n == 0) {
+int sumOfArray(int arr[], int n) {
+    if (n <= 0) {
         return 0;
-    } else if (n == 1) {
-        return 1;
     } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return arr[n - 1] + sumOfArray(arr, n - 1);
     }
 }
 
 int main() {
-    int terms;
-    cout << "Enter the number of terms: ";
-    cin >> terms;
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
 
-    cout << "Fibonacci Series: ";
-    for (int i = 0; i < terms; i++) {
-        cout << fibonacci(i) << " ";
+    int arr[n];
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    cout << endl;
+
+    int sum = sumOfArray(arr, n);
+    cout << "Sum of array elements: " << sum << endl;
 
     return 0;
 }
-
