9ac07cadf70be9f6c987588510f16dfd1d658197
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-11 15:47:55 +0530

    modify

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -53,30 +53,30 @@
 
 //sum of array elements
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int sumOfArray(int arr[], int n) {
-    if (n <= 0) {
-        return 0;
-    } else {
-        return arr[n - 1] + sumOfArray(arr, n - 1);
-    }
-}
+// int sumOfArray(int arr[], int n) {
+//     if (n <= 0) {
+//         return 0;
+//     } else {
+//         return arr[n - 1] + sumOfArray(arr, n - 1);
+//     }
+// }
 
-int main() {
-    int n;
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
+// int main() {
+//     int n;
+//     cout << "Enter the number of elements in the array: ";
+//     cin >> n;
 
-    int arr[n];
-    cout << "Enter the array elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     int arr[n];
+//     cout << "Enter the array elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
-    int sum = sumOfArray(arr, n);
-    cout << "Sum of array elements: " << sum << endl;
+//     int sum = sumOfArray(arr, n);
+//     cout << "Sum of array elements: " << sum << endl;
 
-    return 0;
-}
+//     return 0;
+// }
