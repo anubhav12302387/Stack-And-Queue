f5d48d907fd8cf995d6075a717112e85a552f2ea
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-14 13:26:45 +0530

    hamoi

---
+++ Tree/Hanoi.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+//1 source
+//2 auxillary
+//3 destination
+
+int moveDisks(int n, char source, char auxillary, char destinations) {
+    if(n==1){
+        cout<<"Move disk 1 from rod"<<source<<" to rod"<<destinations<<endl;
+        return 1;
+    }
+}
+
+
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    int totalMoves = moveDisks(n, '1', '2', '3');
+
+    cout << totalMoves << endl;
+
+    return 0;
+}
