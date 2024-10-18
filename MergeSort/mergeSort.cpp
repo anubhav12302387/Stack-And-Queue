68b1888f28d4d2fa1766264d98ce75a018d52cb7
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-10-18 15:17:06 +0530

    BST

--- MergeSort/mergeSort.cpp
+++ MergeSort/mergeSort.cpp
@@ -1,76 +1,76 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+// void printArray(int arr[], int size) {
+//     for (int i = 0; i < size; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+// }
 
-void conquer(int arr[], int l, int m, int r, int size) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int left[n1], right[n2];
+// void conquer(int arr[], int l, int m, int r, int size) {
+//     int n1 = m - l + 1;
+//     int n2 = r - m;
+//     int left[n1], right[n2];
 
-    for (int i = 0; i < n1; i++)
-        left[i] = arr[l + i];
-    for (int i = 0; i < n2; i++)
-        right[i] = arr[m + 1 + i];
+//     for (int i = 0; i < n1; i++)
+//         left[i] = arr[l + i];
+//     for (int i = 0; i < n2; i++)
+//         right[i] = arr[m + 1 + i];
 
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
-            i++;
-        } else {
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
-    }
+//     int i = 0, j = 0, k = l;
+//     while (i < n1 && j < n2) {
+//         if (left[i] <= right[j]) {
+//             arr[k] = left[i];
+//             i++;
+//         } else {
+//             arr[k] = right[j];
+//             j++;
+//         }
+//         k++;
+//     }
 
-    while (i < n1) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
+//     while (i < n1) {
+//         arr[k] = left[i];
+//         i++;
+//         k++;
+//     }
 
-    while (j < n2) {
-        arr[k] = right[j];
-        j++;
-        k++;
-    }
+//     while (j < n2) {
+//         arr[k] = right[j];
+//         j++;
+//         k++;
+//     }
 
-    printArray(arr, size);
-}
+//     printArray(arr, size);
+// }
 
-void divide(int arr[], int l, int r, int size, int* iteration) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
+// void divide(int arr[], int l, int r, int size, int* iteration) {
+//     if (l < r) {
+//         int m = l + (r - l) / 2;
 
-        divide(arr, l, m, size, iteration);
-        divide(arr, m + 1, r, size, iteration);
+//         divide(arr, l, m, size, iteration);
+//         divide(arr, m + 1, r, size, iteration);
 
-        printf("After iteration %d\n", ++(*iteration));
-        conquer(arr, l, m, r, size);
-    }
-}
+//         printf("After iteration %d\n", ++(*iteration));
+//         conquer(arr, l, m, r, size);
+//     }
+// }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//         scanf("%d", &arr[i]);
 
-    printf("Given Array\n");
-    printArray(arr, n);
+//     printf("Given Array\n");
+//     printArray(arr, n);
 
-    int iteration = 0;
-    divide(arr, 0, n - 1, n, &iteration);
+//     int iteration = 0;
+//     divide(arr, 0, n - 1, n, &iteration);
 
-    printf("Sorted Array\n");
-    printArray(arr, n);
+//     printf("Sorted Array\n");
+//     printArray(arr, n);
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ Tree/BT.cpp
@@ -0,0 +1,125 @@
+// #include <iostream>
+
+// using namespace std;
+
+
+// struct Node {
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int val) : data(val), left(nullptr), right(nullptr) {}
+// };
+
+
+// class BinaryTree {
+// public:
+//     Node* root;
+
+//     BinaryTree() : root(nullptr) {}
+
+
+//     void insert(int val) {
+//         root = insertRec(root, val);
+//     }
+
+
+//     Node* insertRec(Node* node, int val) {
+//         if (node == nullptr) {
+//             return new Node(val);
+//         }
+
+//         if (val < node->data) {
+//             node->left = insertRec(node->left, val);
+//         } else {
+//             node->right = insertRec(node->right, val);
+//         }
+//         return node;
+//     }
+
+
+//     void inOrder() {
+//         inOrderRec(root);
+//         cout << endl;
+//     }
+
+
+//     void inOrderRec(Node* node) {
+//         if (node != nullptr) {
+//             inOrderRec(node->left);
+//             cout << node->data << " ";
+//             inOrderRec(node->right);
+//         }
+//     }
+
+
+//     void preOrder() {
+//         preOrderRec(root);
+//         cout << endl;
+//     }
+
+
+//     void preOrderRec(Node* node) {
+//         if (node != nullptr) {
+//             cout << node->data << " ";
+//             preOrderRec(node->left);
+//             preOrderRec(node->right);
+//         }
+//     }
+
+
+//     void postOrder() {
+//         postOrderRec(root);
+//         cout << endl;
+//     }
+
+
+//     void postOrderRec(Node* node) {
+//         if (node != nullptr) {
+//             postOrderRec(node->left);
+//             postOrderRec(node->right);
+//             cout << node->data << " ";
+//         }
+//     }
+// };
+
+
+// int main() {
+//     BinaryTree tree;
+
+  
+//     tree.insert(5);
+//     tree.insert(3);
+//     tree.insert(8);
+//     tree.insert(1);
+//     tree.insert(4);
+//     tree.insert(7);
+//     tree.insert(9);
+
+
+//     cout << "In-Order Traversal: ";
+//     tree.inOrder();
+
+//     cout << "Pre-Order Traversal: ";
+//     tree.preOrder();
+
+//     cout << "Post-Order Traversal: ";
+//     tree.postOrder();
+
+//     return 0;
+// }
+
+
+
+
+// static question
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    return 0;
+}
